3d4b1313a3f31d485f9d95c7920f1cbb8af8e5aa
jarathomas <jarathomas@gmail.com>
2019-02-15 17:40:30 -0500

    Created plot for glob tossing example.

--- Notes/jasonCh2.R
+++ Notes/jasonCh2.R
@@ -34,8 +34,16 @@ d %>%
 
 #### ---- globePlot
 x <- seq(0,1, length=1000)
+outcomes <- c('W', 'L', 'W', 'W', 'W', 'L', 'W', 'L', 'W')
 a <- c(1, 2, 2, 3, 4, 5, 5, 6, 6, 7)
 b <- c(1, 1, 2, 2, 2, 2, 3, 3, 4, 4)
-plot(x, dbeta(x, shape1 = a[1], shape2 = b[1]))
-plot(x, dbeta(x, shape1 = a[2], shape2 = b[2]))
-plot(x, dbeta(x, shape1 = a[3], shape2 = b[3]))
+par(mfrow = c(3,3))
+for (i in 2:10){
+  plot(x, dbeta(x, shape1 = a[i], shape2 = b[i]), ylim = c(0,3),
+       xlab = 'parameter value', ylab = 'density', bty ='n')
+  points(x, dbeta(x, shape1 = a[i-1], shape2 = b[i-1]), col = 'grey')
+  points(x, dbeta(x, shape1 = a[i], shape2 = b[i]))
+  mark <- c(rep(1,i-2), rep(2,1), rep(1,10-i))
+  text(seq(.1, .9, by = .1), 2.9, outcomes, font = mark,
+       col = recode(mark, '1' = 'grey', '2' = 'black'))
+ }

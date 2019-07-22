0e112e75e4bf2f126ccd6542fcaff9b43ffa0042
Jason Thomas <jarathomas@gmail.com>
2019-07-22 00:31:41 -0400

    editing plot

--- Python/ch2.py
+++ Python/ch2.py
@@ -11,31 +11,39 @@ rc('text',usetex=True)
 rc('text.latex', preamble=r'\usepackage{color}')
 
 # 2.2: Building a model: binomial likelihood with (flat) beta prior
-# data: WLWWWLWLW
-a0 = b0 = 1
-beta.stats(a0, b0, moments='mvsk')    # mean, var, skew, kurtosis (mvsk)
+data22 = ['W', 'L', 'W', 'W', 'W', 'L', 'W', 'L', 'W']
+aPost = np.cumsum([1 if x == 'W' else 0 for x in data22]) + 1
+aPost.shape
+a = np.insert(aPost, 0, 1)
+bPost = np.cumsum([0 if x == 'W' else 1 for x in data22]) + 1
+b = np.insert(bPost, 0, 1)
+
+beta.stats(a[0], b[0], moments='mvsk')    # mean, var, skew, kurtosis (mvsk)
 
 # update: {[p ^ x] * [(1 - p) ^ (n - x)]} * {[ p ^ (a - 1)] * [(1-p) ^ (b - 1)]} / B(a,b)
 pRange = np.arange(0, 1, 0.01)
-post0 = beta.pdf(pRange, a0, b0)
-a1 = a0 + 1
-b1 = b0
-a2 = a1 + 0
-b2 = b1 + 1
+
+yVals = beta.pdf(pRange, a[0], b[0])
+for i in range(1, len(a)):
+    yVals = np.append(yVals, beta.pdf(pRange, a[i], b[i]))
+yMax = max(yVals)
 
 plt.figure(1)
-plt.subplot(331)
-plt.plot(pRange, beta.pdf(pRange, a0, b0), 'b--',
-         pRange, beta.pdf(pRange, a1, b1), 'r-')
-plt.ylabel('plausibility')
-plt.title(r'\textcolor{red}{W} ' + r'\textcolor{black}{L W W W L W L W}', fontsize=8)
-plt.text(.05, max(beta.pdf(pRange, a1, b1)) - .15, 'n = 1')
-plt.subplot(332)
-plt.plot(pRange, beta.pdf(pRange, a1, b1), 'b--',
-         pRange, beta.pdf(pRange, a2, b2), 'r-')
-plt.title(r'\textcolor{black}{W} ' +
-          r'\textcolor{red}{L} ' +
-          r'\textcolor{black}{W W W L W L W}', fontsize=8)
-plt.text(.05, max(beta.pdf(pRange, a1, b1)) - .15, 'n = 2')
+for i in range(0, len(a) - 1):
+    plotloc = '33' + str(i + 1)
+    plt.subplot(plotloc)
+    plt.plot(pRange, beta.pdf(pRange, a[i], b[i]), 'b--',
+             pRange, beta.pdf(pRange, a[i + 1], b[i + 1]), 'r-')
+    plt.ylabel('plausibility')
+    plt.axis([0, 1, 0, yMax + .1])
+    plt.title(r'\textcolor{red}{W} ' + r'\textcolor{black}{L W W W L W L W}', fontsize=8)
+    plt.text(.05, yMax - .2, 'n = ' + str(i + 1))
+    # plt.subplot(332)
+    # plt.plot(pRange, beta.pdf(pRange, a1, b1), 'b--',
+    #          pRange, beta.pdf(pRange, a2, b2), 'r-')
+    # plt.title(r'\textcolor{black}{W} ' +
+    #           r'\textcolor{red}{L} ' +
+    #           r'\textcolor{black}{W W W L W L W}', fontsize=8)
+    # plt.text(.05, max(beta.pdf(pRange, a1, b1)) - .15, 'n = 2')
 plt.show()
 plt.savefig('fig22a.pdf')
